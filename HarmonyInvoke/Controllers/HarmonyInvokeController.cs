29e3ad32dd836e2d921e988dc40c859541bcfde7
Steve McKisic <smckisic@dsicovery.com>
2024-07-02 23:48:46 -0500

    Add project files.

---
+++ HarmonyInvoke/Controllers/HarmonyInvokeController.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyInvokeInfrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HarmonyInvoke.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HarmonyInvokeController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public HarmonyInvokeController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("load")]
+        public ActionResult GameLoad(string romName, string bankSwitchingCode)
+        {
+            var result = "Result:";
+            try
+            {
+                if (string.IsNullOrWhiteSpace(romName) || string.IsNullOrWhiteSpace(bankSwitchingCode))
+                {
+                    throw new Exception("Rom Name or Bankswitching Code missing");
+                }
+
+                result = $"{result} {Invoker.Load(romName, bankSwitchingCode)}";
+            }
+            catch (Exception ex)
+            {
+                result = $"ERROR: {ex.Message}";
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ HarmonyInvokeInfrastructure/Invoker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace HarmonyInvokeInfrastructure
+{
+    public static class Invoker
+    {
+        public const string exe = "HarmonyCart";
+        public static string BasePath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        public static string HarmonyPath = Path.Combine(BasePath, "Harmony");
+
+        public static string RomsPath = Path.Combine(HarmonyPath, "ROMS");
+        public static string ExePath = Path.Combine(HarmonyPath, exe);
+
+        public static string Load(string rom, string bankswitchCode) {
+
+            var arguments = $"-bs={bankswitchCode} \"{RomsPath}\\{rom}.BIN\"";
+            var command = $"{ExePath}";
+
+            var processInfo = new ProcessStartInfo(command, arguments);
+            //}
+            //    //RedirectStandardOutput = true,
+            //    //RedirectStandardError = true,
+            //    UseShellExecute = false,
+            //    CreateNoWindow = true                
+            //};
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+
+            //var process = new Process() {
+            //    //EnableRaisingEvents = true,
+            //    StartInfo = processInfo,                
+            //};
+
+            var process = Process.Start(processInfo);
+
+            string output = "";
+            //process.OutputDataReceived += (sender, e) => { var outputX = e.Data; output = outputX; };
+
+            //string error = "";
+            //process.ErrorDataReceived += (sender, e) => { var errorX = e.Data; error = errorX; };
+
+            //process.Start();
+
+            //process.BeginOutputReadLine();
+            //process.BeginErrorReadLine();
+
+            process.WaitForExit();
+            //process.Close();
+
+            return output; // !string.IsNullOrWhiteSpace(error) ? error : output;
+        }
+    }
+}
